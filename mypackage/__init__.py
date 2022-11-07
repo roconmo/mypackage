35f7ca9fd0a5fb8cef3323d1100790c410a884a4
MOREIRA Rosalia <rosalia.contreras@ext.soprasteria.com>
2022-11-07 19:13:19 +0100

    Nothing to declare

---
+++ mypackage/__init__.py
@@ -0,0 +1,3 @@
+__title__ = 'my_package'
+__version__ = '0.0.1'
+__author__ = 'myself'
---
+++ mypackage/operator.py
@@ -0,0 +1,13 @@
+class Operator:
+    def __init__(self, num1:int, num2:int) -> None:
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return self.num1 + self.num2
+
+    def substract(self, inverse: bool = False):
+        if inverse:
+            return self.num2 - self.num1
+        return self.num1 - self.num2
+
